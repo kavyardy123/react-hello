43a1b89341b6d3c2f6693ea360dbba52102c890f
kavyardy123 <kavyardy5240@gmail.com>
2021-06-04 07:39:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-mzdpvv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mzdpvv)
---
+++ src/components.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Login extends React.component {
+  render() {
+    return (
+      <div>
+        <h1> I am Login component</h1>
+      </div>
+    );
+  }
+}
+export default Login;
